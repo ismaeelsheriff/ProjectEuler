aa645fb93e054ddf52d1eb00e805dceba0434a02
ismaeelsheriff <isherif2@illinois.edu>
2015-04-01 01:43:38 -0500

    10update

--- Euler10.java
+++ Euler10.java
@@ -9,7 +9,7 @@ public class Euler10 {
 		int comp = 0;
 		long sum = 2;
 		
-		for (long i = 3; i < end; i++) { //starts at 2 and stops when end# of primes
+		for (long i = 3; i < end; i++, comp=0) { //starts at 2 and stops when end# of primes
 			for (int j = 2; j <= (long)Math.sqrt(i); j++) { //checks divisors from 2 to sqrt of i
 				if (i%j==0) {
 					comp++;
@@ -17,10 +17,8 @@ public class Euler10 {
 			}
 			
 			if (comp == 0) {
-				//System.out.println(i);
 				sum+=i;
 			}
-			comp = 0;
 		}
 		System.out.println(sum);
 	}
